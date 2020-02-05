1c4c8eb2338d16161ad20a493f2389054d6849d5
Kaito Suzuki <kaito.titech@gmail.com>
2020-02-05 18:51:09 +0900

    Fix ESLint

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,11 +3,11 @@ import { useSelector, useDispatch } from "react-redux";
 import loginModule from "../modules/loginModule";
 import { RootState } from "../modules/rootState";
 interface LoginProps {}
-const Login: React.FC<LoginProps> = props => {
+const Login: React.FC<LoginProps> = _props => {
   const dispatch = useDispatch();
   const email = useSelector((state: RootState) => state.login.email);
   const [inputEmail, setInputEmail] = useState("");
-  const submit = (e: React.FormEvent<HTMLButtonElement>) => {
+  const submit = (e: React.FormEvent<HTMLButtonElement>): void => {
     e.preventDefault();
     if (inputEmail !== "") {
       dispatch(loginModule.actions.submit(inputEmail));
@@ -22,7 +22,7 @@ const Login: React.FC<LoginProps> = props => {
       <form>
         <input
           type="text"
-          onChange={e => {
+          onChange={(e): void => {
             setInputEmail(e.target.value);
           }}
           value={inputEmail}
--- src/modules/loginModule.ts
+++ src/modules/loginModule.ts
@@ -12,7 +12,7 @@ const loginModule = createSlice({
   name: "login",
   initialState: loginInitialState,
   reducers: {
-    submit: (state, action: PayloadAction<string>) => {
+    submit: (state, action: PayloadAction<string>): void => {
       state.email = action.payload;
     }
   }
